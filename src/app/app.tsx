bf23fa9696fce0d8932de92078fbdcad546bc11c
GitBrent <gitbrent@users.noreply.github.com>
2019-02-21 23:37:09 -0600

    linted code; more work on "add new" dialog

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -29,277 +29,342 @@
 
 // TODO: https://github.com/FortAwesome/react-fontawesome
 
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import APP_LOGO_BASE64 from '../img/logo_base64';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import APP_LOGO_BASE64 from '../img/logo_base64'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 interface IDream {
-	dreamTitle: string;
-	dreamDetails?: string;
-	lucidType?: 'dild' | 'mild' | 'wbtb' | 'other';
+	title: string
+	notes?: string
+	dreamSigns?: Array<string>
+	lucidDream?: boolean
+	lucidMethod?: 'dild' | 'mild' | 'wbtb' | 'other'
 }
 interface IDailyEntry {
-	entryDate: Date;
-	bedTime?: Date;
-	notesPrep?: string;
-	notesWake?: string;
-	dreamSigns?: Array<string>;
-	dreams?: Array<IDream>;
+	entryDate: Date
+	bedTime?: Date
+	notesPrep?: string
+	notesWake?: string
+	dreams?: Array<IDream>
 }
 
-var dreamsJson = null;
+var dreamsJson = null
 try {
-	dreamsJson = require('../data/dreams.json');
-}
-catch(ex) {
-	console.log('FYI: Unable to open `dreams.json` data file. (probably okay is this is first run)');
-	console.log(ex);
+	// TODO: check for `data` directory
+	dreamsJson = require('../data/dreams.json')
+} catch (ex) {
+	console.log('FYI: Unable to open `dreams.json` data file. (probably okay is this is first run)')
+	console.log(ex)
 }
 
 // TODO: https://reactjs.org/docs/forms.html
 // FYI: https://stackoverflow.com/questions/24502898/show-or-hide-element-in-react
 
 class AppNavBar extends React.Component<{ onChange?: Function }> {
-	constructor(props: Readonly<{ onChange?: Function; }>) {
-		super(props);
+	constructor(props: Readonly<{ onChange?: Function }>) {
+		super(props)
 	}
 
-	changeHandler = (e) => {
-        if (typeof this.props.onChange === 'function') {
+	changeHandler = e => {
+		if (typeof this.props.onChange === 'function') {
 			//this.props.onChange(e.target.value);
 			//this.props.onChange('TAB1');
-            this.props.onChange(true);
-        }
-    }
+			this.props.onChange(true)
+		}
+	}
 
 	render() {
 		return (
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<a className="navbar-brand" href="#">
-					<img src={APP_LOGO_BASE64} width="30" height="30" className="d-inline-block align-top mr-3" alt="" />
+			<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+				<a className='navbar-brand' href='#'>
+					<img
+						src={APP_LOGO_BASE64}
+						width='30'
+						height='30'
+						className='d-inline-block align-top mr-3'
+						alt=''
+					/>
 					Dream Journal App
 				</a>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-toggle='collapse'
+					data-target='#navbarNav'
+					aria-controls='navbarNav'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span className='navbar-toggler-icon' />
 				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav">
-						<li className="nav-item active">
-							<a className="nav-link" href="#Home">Home <span className="sr-only">(current)</span></a>
+				<div className='collapse navbar-collapse' id='navbarNav'>
+					<ul className='navbar-nav'>
+						<li className='nav-item active'>
+							<a className='nav-link' href='#Home'>
+								Home <span className='sr-only'>(current)</span>
+							</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#Search">Search Dreams</a>
+						<li className='nav-item'>
+							<a className='nav-link' href='#Search'>
+								Search Dreams
+							</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#Add">Add New Dreams</a>
+						<li className='nav-item'>
+							<a className='nav-link' href='#Add'>
+								Add New Dreams
+							</a>
 						</li>
 					</ul>
 				</div>
-				<form className="form-inline mb-0">
-					<button type="button" onClick={this.changeHandler} className="btn btn-outline-primary mr-2">Load Data File</button>
-					<button className="btn btn-outline-success" type="button" disabled>Save Data File</button>
+				<form className='form-inline mb-0'>
+					<button type='button' onClick={this.changeHandler} className='btn btn-outline-primary mr-2'>
+						Load Data File
+					</button>
+					<button className='btn btn-outline-success' type='button' disabled>
+						Save Data File
+					</button>
 				</form>
 			</nav>
-		);
+		)
 	}
 }
 
 class TabHome extends React.Component {
 	constructor(props) {
-		super(props);
+		super(props)
 	}
 
 	render() {
-		if ( !dreamsJson ) {
+		if (!dreamsJson) {
 			return (
-				<div className="container mt-5">
-					<div className="jumbotron">
-						<h1 className="display-4 text-primary mb-3">
-							<img src={APP_LOGO_BASE64} width="150" height="150" className="mr-4" alt="Logo" />Dream Journal App
+				<div className='container mt-5'>
+					<div className='jumbotron'>
+						<h1 className='display-4 text-primary mb-3'>
+							<img src={APP_LOGO_BASE64} width='150' height='150' className='mr-4' alt='Logo' />
+							Dream Journal App
 						</h1>
-						<p className="lead">Record your daily dream journal entries into well-formatted JSON.</p>
-						<hr className="my-4" />
-						<p>This enables metrics, keyword searches and much more so you can make the best of your dreams.</p>
-						<a className="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
+						<p className='lead'>Record your daily dream journal entries into well-formatted JSON.</p>
+						<hr className='my-4' />
+						<p>
+							This enables metrics, keyword searches and much more so you can make the best of your
+							dreams.
+						</p>
+						<a className='btn btn-primary btn-lg' href='#' role='button'>
+							Get Started
+						</a>
 					</div>
 				</div>
-			);
-		}
-		else {
-			return (
-				<h1>You Are a Dream God!</h1>
 			)
+		} else {
+			return <h1>You Are a Dream God!</h1>
 		}
 	}
 }
 
 class TabSearch extends React.Component {
 	constructor(props) {
-		super(props);
+		super(props)
 	}
 
 	render() {
 		return (
-			<div className="row align-items-end justify-content-between">
-				<div className="col-auto">
-					<h1 className="text-primary">Dream Journal App</h1>
+			<div className='row align-items-end justify-content-between'>
+				<div className='col-auto'>
+					<h1 className='text-primary'>Dream Journal App</h1>
 				</div>
-				<div className="col-auto">
-					<h6 id="appVer" className="text-black-50 font-weight-light"></h6>
+				<div className='col-auto'>
+					<h6 id='appVer' className='text-black-50 font-weight-light' />
 				</div>
 			</div>
-		);
+		)
 	}
 }
 
-class AppModal extends React.Component<{ show?: boolean }, { show: boolean }> {
-	private dreams: [{}];
-
-	constructor(props: Readonly<{ show?: boolean; }>) {
-		super(props);
+class AppModal extends React.Component<{ show?: boolean }, { show: boolean, dailyEntry: IDailyEntry }> {
+	constructor(props: Readonly<{ show?: boolean }>) {
+		super(props)
 
 		this.state = {
-			show: props.show
-		};
+			show: props.show,
+			dailyEntry: {
+				entryDate: null,
+				bedTime: null,
+				notesPrep: null,
+				notesWake: null,
+				dreams: [ {title:""} ]
+			}
+		}
 	}
 
 	// React-Design: Allow `props` changes from other Components to change state/render
 	componentWillReceiveProps(nextProps) {
-		const newName = nextProps.show;
-		if ( this.state.show !== newName ) {
-			this.setState({ show:newName });
+		const newName = nextProps.show
+		if (this.state.show !== newName) {
+			this.setState({ show: newName })
 		}
 	}
 
-	// TODO: handle "new dream row" button click
-	changeHandler = (e) => {
-		console.log('click!');
-		// TODO: (here): 1-add node to dreams, 2-render()
+	addRowHandler = e => {
+		this.state.dailyEntry.dreams.push({ title:"" })
+		console.log(this.state.dailyEntry.dreams)
+		this.render() // no affect???
+		console.log('addRowHandler!')
+		// TODO: CURR: the dialog rows just wont re-render once theyre onscreen!!!
+	}
 
-		// for `return` below
-		/*
-		<div className="blocks_loop">
-          {this.props.dreams.map(block => (
-            <div className="block" />
-          ))}
-        </div>
-		*/
+	changeHandler = e => {
+		console.log('form field changed!')
 	}
 
 	render() {
 		let modalClose = () => {
-			this.setState({ show: false });
+			this.setState({ show: false })
 		}
 
 		return (
-			<Modal size="lg" show={ this.state.show } onHide={ modalClose } >
-			  <Modal.Header className="bg-primary" closeButton>
-			  	<Modal.Title className="text-white">Journal Entry</Modal.Title>
-			  </Modal.Header>
+			<Modal size='lg' show={this.state.show} onHide={modalClose}>
+				<Modal.Header className='bg-primary' closeButton>
+					<Modal.Title className='text-white'>Journal Entry</Modal.Title>
+				</Modal.Header>
 
-			  <Modal.Body className="bg-light">
-				  <div className="container mb-3">
-					  <div className="row mb-3">
-						  <div className="col-12 col-md-6">
-							  <label className="text-muted text-uppercase text-sm">Entry Date</label>
-							  <input id="entryDate" type="date" className="form-control w-50" />
-						  </div>
-						  <div className="col-12 col-md-6">
-							  <label className="text-muted text-uppercase text-sm">Bed Time</label>
-							  <input id="bedTime" type="time" className="form-control w-50" />
-						  </div>
-					  </div>
-					  <div className="row">
-						  <div className="col-12 col-md-6">
-							  <label className="text-muted text-uppercase text-sm">Prep Notes</label>
-							  <textarea id="notesPrep" className="form-control" style={{ height:"80px" }}></textarea>
-						  </div>
-						  <div className="col-12 col-md-6">
-							  <label className="text-muted text-uppercase text-sm">Wake Notes</label>
-							  <textarea id="notesWake" className="form-control" style={{ height:"80px" }}></textarea>
-						  </div>
-					  </div>
-				  </div>
+				<Modal.Body className='bg-light'>
+					<div className='container mb-4'>
+						<div className='row mb-3'>
+							<div className='col-12 col-md-6 required'>
+								<label className='text-muted text-uppercase text-sm'>Entry Date</label>
+								<input id='entryDate' type='date' className='form-control w-50' required />
+								<div className='invalid-feedback'>Please provide Entry Date</div>
+							</div>
+							<div className='col-12 col-md-6'>
+								<label className='text-muted text-uppercase text-sm'>Bed Time</label>
+								<input id='bedTime' type='time' className='form-control w-50' />
+							</div>
+						</div>
+						<div className='row'>
+							<div className='col-12 col-md-6'>
+								<label className='text-muted text-uppercase text-sm'>Prep Notes</label>
+								<textarea id='notesPrep' className='form-control' rows={3} />
+							</div>
+							<div className='col-12 col-md-6'>
+								<label className='text-muted text-uppercase text-sm'>Wake Notes</label>
+								<textarea id='notesWake' className='form-control' rows={3} />
+							</div>
+						</div>
+					</div>
 
-				  <div className="container">
-				  	<h5 className="text-primary">Dream(s)</h5>
-/* start loop over `dreams` */
-					  <div className="row">
-						  <div className="col">
-							  <label className="text-black-50 text-sm">Add new dreams as needed</label>
-							  <div className="row mb-3" data-dreamidx="0">
-								  <div className="col-auto">
-								  	<button className="btn btn-sm btn-outline-info" onClick={this.changeHandler}>Add</button>
-								  </div>
-								  <div className="col">
-									  <label className="text-muted text-uppercase text-sm">Title</label>
-									  <input id="title" type="text" className="form-control" />
-								  </div>
-							  </div>
-						  </div>
-					  </div>
-/* end loop */
-				  </div>
-			  </Modal.Body>
+					<div className='container'>
+						<div className='row mb-3'>
+							<div className='col'>
+								<h5 className='text-primary'>Dreams</h5>
+							</div>
+							<div className='col-auto'>
+								<button className='btn btn-sm btn-outline-info' onClick={this.addRowHandler}>
+									Add Dream Row
+								</button>
+							</div>
+						</div>
+						{this.state.dailyEntry.dreams.map((dream, idx) => {
+							console.log(idx)
+							return <div className='row' key={idx}>
+								<div className='col-auto border-right'>
+									<h2 className='text-primary font-weight-light'>{idx + 1}</h2>
+								</div>
+								<div className='col'>
+									<div className='row mb-3'>
+										<div className='col'>
+											<label className='text-muted text-uppercase text-sm'>Title</label>
+											<input
+												id='title'
+												type='text'
+												className='form-control'
+												value={dream.title}
+												onChange={this.changeHandler}
+											/>
+										</div>
+										<div className='col-auto'>
+											<label className='text-muted text-uppercase text-sm d-block'>Lucid Dream?</label>
+											<span>TODO-bs4-toggle</span>
+										</div>
+										<div className='col-auto'>
+											<label className='text-muted text-uppercase text-sm d-block'>Lucid Method</label>
+											<select className='form-control'><option>Select...</option></select>
+										</div>
+									</div>
+									<div className='row'>
+										<div className='col'>
+											<label className='text-muted text-uppercase text-sm'>Notes</label>
+											<textarea
+												id='notes'
+												className='form-control'
+												rows={5}
+												value={dream.notes}
+												onChange={this.changeHandler}
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+						})}
+					</div>
+				</Modal.Body>
 
-			  <Modal.Footer>
-			    <Button variant="secondary" className="px-4 mr-2" onClick={modalClose}>Close</Button>
-			    <Button variant="primary" className="w-25">Submit</Button>
-			  </Modal.Footer>
-	        </Modal>
-		);
+				<Modal.Footer>
+					<Button variant='secondary' className='px-4 mr-2' onClick={modalClose}>
+						Close
+					</Button>
+					<Button variant='primary' className='w-25'>
+						Submit
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		)
 	}
 }
 
 // TODO: move to Component and accept a PROP for which tab to show!!!
 function ShowAppTab() {
-	if ( window.location.href.toLowerCase().indexOf('#search') > -1 ) {
-		return <TabSearch />;
-	}
-	else if ( window.location.href.toLowerCase().indexOf('#new') > -1 ) {
+	if (window.location.href.toLowerCase().indexOf('#search') > -1) {
+		return <TabSearch />
+	} else if (window.location.href.toLowerCase().indexOf('#new') > -1) {
 		//return <TabNew />;
-	}
-	else {
-		return <TabHome />;
+	} else {
+		return <TabHome />
 	}
 }
 
 // APP UI
 class AppUI extends React.Component<{}, { showModal: boolean }> {
 	constructor(props) {
-		super(props);
+		super(props)
 
 		this.state = {
-			showModal: props.showModal || false
-		};
+			showModal: props.showModal || false,
+		}
 	}
 
-    chgShowModal = (value) => {
+	chgShowModal = value => {
 		this.setState({
-			showModal: value
-		});
-    }
+			showModal: value,
+		})
+	}
 
-    render() {
-		console.log("MAIN-RENDER: this.state.showModal = "+ this.state.showModal);
+	render() {
+		console.log('MAIN-RENDER: this.state.showModal = ' + this.state.showModal)
 		return (
 			<main>
 				<AppNavBar onChange={this.chgShowModal} />
 				<ShowAppTab />
 				<AppModal show={this.state.showModal} />
 			</main>
-		);
+		)
 	}
 }
 
 // AppMain
-const AppMain: React.SFC<{ compiler: string, framework: string }> = (props) => {
-	return (<AppUI />);
+const AppMain: React.SFC<{ compiler: string; framework: string }> = props => {
+	return <AppUI />
 }
 
-ReactDOM.render(
-	<AppMain compiler="TypeScript" framework="React" />,
-	document.getElementById("root")
-);
+ReactDOM.render(<AppMain compiler='TypeScript' framework='React' />, document.getElementById('root'))
