1b7a37db74bf3b27e9b94dcd31fc827e5f0195da
GitBrent <gitbrent@users.noreply.github.com>
2019-03-06 21:40:10 -0600

    added css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,57 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: './src/app/app.tsx',
-	mode: 'development',
-	plugins: [
-		new CleanWebpackPlugin(['public/build']),
-		new HtmlWebpackPlugin({
-			template: 'src/templates/index.html'
-		}),
-	],
-	output: {
-		path: __dirname + '/public',
-		filename: 'build/[name].[contenthash].js'
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.json']
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader'
-			},
-			{
-				test: /\.json$/,
-				loader: 'json-loader'
-			},
-			{
-			    test: /\.svg$/,
-				exclude: /(node_modules|bower_components)/,
-			    loader: 'svg-inline-loader?classPrefix',
-				options: {
-					jsx: true // true outputs JSX tags
-				}
-			},
-			{
-				test: /\.(gif|png|jpe?g|svg)$/i,
-				use: [
-					'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							bypassOnDebug: true, // webpack@1.x
-							disable: true, // webpack@2.x and newer
-						}
-					}
-				]
-			},
-		]
-	}
-}
+  entry: "./src/app/app.tsx",
+  mode: "development",
+  plugins: [
+    new CleanWebpackPlugin(["public/build"]),
+    new HtmlWebpackPlugin({
+      template: "src/templates/index.html"
+    })
+  ],
+  output: {
+    path: __dirname + "/public",
+    filename: "build/[name].[contenthash].js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "css-loader"
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+      {
+        test: /\.svg$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "svg-inline-loader?classPrefix",
+        options: {
+          jsx: true // true outputs JSX tags
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true // webpack@2.x and newer
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
