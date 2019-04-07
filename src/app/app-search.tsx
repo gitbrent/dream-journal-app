437b66a8857b270c63de4df5e7bd6aa60af6caca
Brent Ely <gitbrent@gmail.com>
2019-04-06 23:34:40 -0500

    more UI work

--- src/app/app-search.tsx
+++ src/app/app-search.tsx
@@ -8,8 +8,33 @@ export default class TabSearch extends React.Component<{ selDataFile: IDriveFile
 
 	render() {
 		return (
-			<div className='container mt-3'>
-				<h2 className='text-primary mb-3'>Search Dream Journal</h2>
+			<div className='container mt-5'>
+				<div className='card mb-5'>
+					<div className='card-header bg-primary'>
+						<h5 className='card-title text-white mb-0'>Search / Analyize Dream Journal Entries</h5>
+					</div>
+					<div className='card-body bg-light'>
+						<div className='row align-items-top'>
+							<div className='col-auto'>
+								<div className='iconSvg size96 wizard' />
+							</div>
+							<div className='col'>
+								<p className='card-text'>
+									The best feature of a well-formatted dream journal is the ability to analyze its
+									contents.
+								</p>
+								<p className='card-text'>
+									How many lucid dreams have you had?
+									<br />
+									What are your common dream signs?
+									<br />
+									How many times have you dreamed about school?
+									<br />
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 
 				<div className='row bg-light p-4'>
 					<div className='col text-center border-secondary border-right'>
@@ -17,7 +42,7 @@ export default class TabSearch extends React.Component<{ selDataFile: IDriveFile
 						<h1 className='text-primary'>
 							{this.props.selDataFile && this.props.selDataFile.entries
 								? this.props.selDataFile.entries.length
-								: '?'}
+								: '-'}
 						</h1>
 					</div>
 					<div className='col text-center'>
@@ -31,7 +56,7 @@ export default class TabSearch extends React.Component<{ selDataFile: IDriveFile
 											}).length > 0
 										)
 								  }).length
-								: '0'}
+								: '-'}
 						</h1>
 					</div>
 				</div>
