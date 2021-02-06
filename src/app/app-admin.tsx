4aba5bff35fc97545dbe32d6fcaff56a06d267c2
Brent Ely <gitbrent@users.noreply.github.com>
2021-02-06 15:04:46 -0600

    added Tabs and new badBedtime feature

--- src/app/app-admin.tsx
+++ src/app/app-admin.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { CardDreamSignGrpViewType, IDreamSignTagGroup, IDriveFile, IJournalEntry } from './app.types'
-import { Search } from 'react-bootstrap-icons'
 import DreamSignTag from './comp-app/dreamsign-tag'
+import { InfoCircle, Search } from 'react-bootstrap-icons'
 
 export interface IAppAdminProps {
 	dataFile: IDriveFile
@@ -36,16 +36,19 @@ export default function TabAdmin(props: IAppAdminProps) {
 	const [totalLucid, setTotalLucid] = useState(0)
 	const [totalUntagged, setTotalUntagged] = useState(0)
 	const [totalDreamSigns, setTotalDreamSigns] = useState(0)
+	//
 	const [dreamTagGroups, setDreamTagGroups] = useState<IDreamSignTagGroup[]>([])
-	const [dupeDreamSigns, setDupeDreamSigns] = useState<IJournalEntry[]>([])
 	const [searchTerm, setSearchTerm] = useState('')
 	const [filterViewType, setFilterViewType] = useState<CardDreamSignGrpViewType>(CardDreamSignGrpViewType.md)
 	const [filterSortOrder, setFilterSortOrder] = useState<FilterSortOrder>(FilterSortOrder.title)
+	//
+	const [dupeDreamSigns, setDupeDreamSigns] = useState<IJournalEntry[]>([])
+	const [badBedTimes, setBadBedTimes] = useState<IJournalEntry[]>([])
 
 	useEffect(() => {
 		if (!props.dataFile || !props.dataFile.entries) return
 
-		// METRICS
+		// Total: Months
 		{
 			let d1 = new Date(props.dataFile.entries.sort((a, b) => ((a.entryDate || 'zzz') < (b.entryDate || 'zzz') ? -1 : 1))[0].entryDate)
 			let d2 = new Date(props.dataFile.entries.sort((a, b) => ((a.entryDate || '000') > (b.entryDate || '000') ? -1 : 1))[0].entryDate)
@@ -56,50 +59,70 @@ export default function TabAdmin(props: IAppAdminProps) {
 			months += 2 // include both first and last months
 			setTotalMonths(months <= 0 ? 0 : months)
 		}
+
+		// Total: Years
 		{
 			let d1 = new Date(props.dataFile.entries.sort((a, b) => ((a.entryDate || 'zzz') < (b.entryDate || 'zzz') ? -1 : 1))[0].entryDate)
 			let d2 = new Date(props.dataFile.entries.sort((a, b) => ((a.entryDate || '000') > (b.entryDate || '000') ? -1 : 1))[0].entryDate)
 			setTotalYears(d2.getFullYear() - d1.getFullYear() + 1)
 		}
 
-		setTotalEntries(props.dataFile.entries.length)
-		setTotalDreams(props.dataFile.entries.map((entry) => entry.dreams.length).reduce((a, b) => a + b))
-		setTotalStars(props.dataFile.entries.filter((entry) => entry.starred).length)
-		setTotalLucid(props.dataFile.entries.map((entry) => entry.dreams.filter((dream) => dream.isLucidDream).length).reduce((a, b) => a + b))
-		setTotalUntagged(props.dataFile.entries.map((entry) => entry.dreams.filter((dream) => dream.dreamSigns.length === 0).length).reduce((a, b) => a + b))
+		// Total: all metrics
+		{
+			setTotalEntries(props.dataFile.entries.length)
+			setTotalDreams(props.dataFile.entries.map((entry) => entry.dreams.length).reduce((a, b) => a + b))
+			setTotalStars(props.dataFile.entries.filter((entry) => entry.starred).length)
+			setTotalLucid(props.dataFile.entries.map((entry) => entry.dreams.filter((dream) => dream.isLucidDream).length).reduce((a, b) => a + b))
+			setTotalUntagged(props.dataFile.entries.map((entry) => entry.dreams.filter((dream) => dream.dreamSigns.length === 0).length).reduce((a, b) => a + b))
+		}
 
-		// tags grouped
-		let tagGroups: IDreamSignTagGroup[] = []
-		let dupeSigns: IJournalEntry[] = []
-		props.dataFile.entries
-			.sort((a, b) => (a.entryDate < b.entryDate ? -1 : 1))
-			.forEach((entry) => {
-				entry.dreams.forEach((dream) =>
-					dream.dreamSigns.forEach((sign) => {
-						let tag = tagGroups.filter((tag) => tag.dreamSign === sign)[0]
-						if (tag) {
-							let existingEntry = tag.dailyEntries.filter((item) => item.entryDate == entry.entryDate)[0]
-							if (!existingEntry) tag.dailyEntries.push(entry)
-							tag.totalOccurs++
-						} else {
-							tagGroups.push({ dreamSign: sign, dailyEntries: [entry], totalOccurs: 1 })
-						}
-					})
-				)
+		// Tag Groups /&/ dupe tags /&/ bad bedTime
+		{
+			let tagGroups: IDreamSignTagGroup[] = []
+			let dupeSigns: IJournalEntry[] = []
+			let bedTimes: IJournalEntry[] = []
 
-				// Find dupes
-				if (
-					entry.dreams &&
-					entry.dreams[0] &&
-					entry.dreams[0].dreamSigns.length > 0 &&
-					entry.dreams[1] &&
-					entry.dreams[0].dreamSigns.toString() == entry.dreams[1].dreamSigns.toString()
-				)
-					dupeSigns.push(entry)
-			})
-		setDreamTagGroups(tagGroups)
-		setTotalDreamSigns(tagGroups.length)
-		setDupeDreamSigns(dupeSigns)
+			props.dataFile.entries
+				.sort((a, b) => (a.entryDate < b.entryDate ? -1 : 1))
+				.forEach((entry) => {
+					entry.dreams.forEach((dream) =>
+						dream.dreamSigns.forEach((sign) => {
+							let tag = tagGroups.filter((tag) => tag.dreamSign === sign)[0]
+							if (tag) {
+								let existingEntry = tag.dailyEntries.filter((item) => item.entryDate == entry.entryDate)[0]
+								if (!existingEntry) tag.dailyEntries.push(entry)
+								tag.totalOccurs++
+							} else {
+								tagGroups.push({ dreamSign: sign, dailyEntries: [entry], totalOccurs: 1 })
+							}
+						})
+					)
+
+					// Find dupes
+					if (
+						entry.dreams &&
+						entry.dreams[0] &&
+						entry.dreams[0].dreamSigns.length > 0 &&
+						entry.dreams[1] &&
+						entry.dreams[0].dreamSigns.toString() == entry.dreams[1].dreamSigns.toString()
+					) {
+						dupeSigns.push(entry)
+					}
+
+					// Bad `bedTime` items
+					if (!entry.bedTime || entry.bedTime.length != 5) {
+						bedTimes.push(entry)
+					}
+				})
+
+			// tagGroup
+			setDreamTagGroups(tagGroups)
+			setTotalDreamSigns(tagGroups.length)
+			// dupe
+			setDupeDreamSigns(dupeSigns)
+			// bedtime
+			setBadBedTimes(bedTimes)
+		}
 	}, [props.dataFile])
 
 	function renderHeader(): JSX.Element {
@@ -165,9 +188,9 @@ export default function TabAdmin(props: IAppAdminProps) {
 		)
 	}
 
-	function renderDreamSignTags(): JSX.Element {
+	function renderTagGroups(): JSX.Element {
 		return (
-			<div className='card my-5'>
+			<div className='card'>
 				<div className='card-header bg-secondary text-white'>
 					<div className='row'>
 						<div className='col'>
@@ -178,7 +201,7 @@ export default function TabAdmin(props: IAppAdminProps) {
 						</div>
 					</div>
 				</div>
-				<div className='card-body bg-light border-bottom border-secondary' data-desc="commandbar">
+				<div className='card-body bg-light border-bottom border-secondary' data-desc='commandbar'>
 					<div className='row align-items-center'>
 						<div className='col-auto d-none d-md-block'>
 							<Search size={48} className='text-secondary' />
@@ -218,7 +241,7 @@ export default function TabAdmin(props: IAppAdminProps) {
 						</div>
 					</div>
 				</div>
-				<div className='card-body bg-light' data-desc="tag cards">
+				<div className='card-body bg-light' data-desc='tag cards'>
 					<div className='card-deck'>
 						{dreamTagGroups
 							.filter(
@@ -254,28 +277,78 @@ export default function TabAdmin(props: IAppAdminProps) {
 
 	function renderDupeTags(): JSX.Element {
 		return (
-			<div className='card my-5'>
-				<div className='card-header bg-warning text-white'>
-					<h5 className='mb-0'>Dupe Entries (same Dream Signs across multiple dreams)</h5>
-				</div>
-				<div className='card-body bg-light'>
+			<section>
+				<h5 className='text-primary'>Dupe Entries (same Dream Signs across multiple dreams)</h5>
+
+				<div className='mt-4'>
 					{dupeDreamSigns.map((entry, idx) => (
 						<button key={`tagDupe${idx}`} onClick={() => props.onShowModal({ show: true, editEntry: entry })} className='btn btn-sm btn-secondary mb-2 mr-2'>
 							{entry.entryDate}
 						</button>
 					))}
 				</div>
-			</div>
+			</section>
 		)
 	}
 
-	if (!props.dataFile || !props.dataFile.entries) return <div />
-	else
+	function renderBadDates(): JSX.Element {
 		return (
-			<main className='container'>
-				{renderHeader()}
-				{renderDreamSignTags()}
-				{renderDupeTags()}
-			</main>
+			<section>
+				<h5 className='text-primary'>
+					Bad Bed Times (<code>bedTime</code> is not standard '00:00' format)
+				</h5>
+
+				<div className='mt-4'>
+					{badBedTimes.map((entry, idx) => (
+						<button key={`badTime${idx}`} onClick={() => props.onShowModal({ show: true, editEntry: entry })} className='btn btn-sm btn-secondary mb-2 mr-2'>
+							{entry.bedTime || '(empty)'}
+						</button>
+					))}
+					{badBedTimes.length === 0 && (
+						<div className='alert alert-secondary' role='alert'>
+							<InfoCircle size='16' className='mr-2' />
+							No results
+						</div>
+					)}
+				</div>
+			</section>
 		)
+	}
+
+	return !props.dataFile || !props.dataFile.entries ? (
+		<div />
+	) : (
+		<main className='container'>
+			{renderHeader()}
+
+			<ul className='nav nav-tabs nav-fill' id='adminTab' role='tablist'>
+				<li className='nav-item' role='presentation'>
+					<a className='nav-link active' id='search-tab' data-toggle='tab' href='#search' role='tab' aria-controls='search' aria-selected='true'>
+						Rename DreamSign/Tags
+					</a>
+				</li>
+				<li className='nav-item' role='presentation'>
+					<a className='nav-link' id='dupe-tab' data-toggle='tab' href='#dupe' role='tab' aria-controls='dupe' aria-selected='false'>
+						Fix Dupe Tags
+					</a>
+				</li>
+				<li className='nav-item' role='presentation'>
+					<a className='nav-link' id='bedtime-tab' data-toggle='tab' href='#bedtime' role='tab' aria-controls='bedtime' aria-selected='false'>
+						Fix Bed Time
+					</a>
+				</li>
+			</ul>
+			<div className='tab-content' id='adminTabContent'>
+				<div className='tab-pane show active bg-light p-4' id='search' role='tabpanel' aria-labelledby='search-tab'>
+					{renderTagGroups()}
+				</div>
+				<div className='tab-pane bg-light p-4' id='dupe' role='tabpanel' aria-labelledby='dupe-tab'>
+					{renderDupeTags()}
+				</div>
+				<div className='tab-pane bg-light p-4' id='bedtime' role='tabpanel' aria-labelledby='bedtime-tab'>
+					{renderBadDates()}
+				</div>
+			</div>
+		</main>
+	)
 }
