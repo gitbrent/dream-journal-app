76d3105803800caab1e7d845038cd6b8f30786d5
Brent Ely <gitbrent@users.noreply.github.com>
2021-02-23 19:46:26 -0600

    moved dream metadata enums to sep file

--- src/app/app.types.tsx
+++ src/app/app.types.tsx
@@ -116,13 +116,3 @@ export interface IDreamTagByCat {
 	/** @example ['Action:Crime', 'Action:Military'] */
 	dreamTagGroups: IDreamSignTagGroup[]
 }
-
-export enum NightlyAffirmations {
-	'SMALL' = `Tonight, I will recognize my Dream Symbols and become aware I am dreaming.`,
-	'MILD (Naiya-Style)' = `Relax and completely de-stress.` +
-		`Visualize as best you can the last vivid dream - now imagine yourself becoming Lucid in the dream.` +
-		`Repeat to yourself, "I will have a lucid dream tonight."`,
-	'MILD 1' = `I resolve to become aware in my dreams tonight and have a Lucid Dream.` +
-		`Tonight, I will recognize my Dream-Signs and become aware I am dreaming.` +
-		`I can see myself becoming aware in my dreams and I can see myself surrounded by Dream-Signs.`,
-}
