0869370ab03791fbadb7fe4959117441a145f731
Brent Ely <gitbrent@users.noreply.github.com>
2021-02-23 23:17:36 -0600

    new affir

--- src/app/app.enums.tsx
+++ src/app/app.enums.tsx
@@ -1,5 +1,6 @@
 export enum NightlyAffirmations {
-	'SMALL' = `Tonight, I will recognize my Dream Symbols and become aware I am dreaming.`,
+	'SMALL1' = `Tonight, I will recognize my Dream Symbols and become aware I am dreaming.`,
+	'SMALL2' = `I will become aware during all of my dreams tonight, and retain control for as long as I wish.`,
 	'MILD (Naiya-Style)' = `Relax and completely de-stress.` +
 		`Visualize as best you can the last vivid dream - now imagine yourself becoming Lucid in the dream.` +
 		`Repeat to yourself, "I will have a lucid dream tonight."`,
