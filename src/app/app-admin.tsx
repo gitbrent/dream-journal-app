5360bdb95edab41a2265c07476b9416eee0971b3
Brent Ely <gitbrent@users.noreply.github.com>
2021-03-03 21:35:03 -0600

    switched to `HeaderMetrics`

--- src/app/app-admin.tsx
+++ src/app/app-admin.tsx
@@ -34,6 +34,7 @@ import * as GDrive from './google-oauth'
 import DreamTagCard from './components/dreamtag-card'
 import AlertGdriveStatus from './components/alert-gstat'
 import ModalEntry from './modal-entry'
+import HeaderMetrics from './components/header-metrics'
 
 interface Props {
 	dataFile: IDriveFile
@@ -63,15 +64,6 @@ export default function TabAdmin(props: Props) {
 	const [showModal, setShowModal] = useState(false)
 	const [currEntry, setCurrEntry] = useState<IJournalEntry>(null)
 	//
-	const [totalMonths, setTotalMonths] = useState(0)
-	const [totalYears, setTotalYears] = useState(0)
-	const [totalEntries, setTotalEntries] = useState(0)
-	const [totalDreams, setTotalDreams] = useState(0)
-	const [totalStars, setTotalStars] = useState(0)
-	const [totalLucid, setTotalLucid] = useState(0)
-	const [totalUntagged, setTotalUntagged] = useState(0)
-	const [totalDreamSigns, setTotalDreamSigns] = useState(0)
-	//
 	const [dreamTagGroups, setDreamTagGroups] = useState<IDreamSignTagGroup[]>([])
 	const [searchTerm, setSearchTerm] = useState('')
 	const [filterViewType, setFilterViewType] = useState<CardDreamSignGrpViewType>(CardDreamSignGrpViewType.md)
@@ -83,81 +75,49 @@ export default function TabAdmin(props: Props) {
 	useEffect(() => {
 		if (!props.dataFile || !props.dataFile.entries) return
 
-		// Total: Months
-		{
-			let d1 = new Date(props.dataFile.entries.sort((a, b) => ((a.entryDate || 'zzz') < (b.entryDate || 'zzz') ? -1 : 1))[0].entryDate)
-			let d2 = new Date(props.dataFile.entries.sort((a, b) => ((a.entryDate || '000') > (b.entryDate || '000') ? -1 : 1))[0].entryDate)
-			let months: number
-			months = (d2.getFullYear() - d1.getFullYear()) * 12
-			months -= d1.getMonth() + 1
-			months += d2.getMonth()
-			months += 2 // include both first and last months
-			setTotalMonths(months <= 0 ? 0 : months)
-		}
+		let tagGroups: IDreamSignTagGroup[] = []
+		let dupeSigns: IJournalEntry[] = []
+		let bedTimes: IJournalEntry[] = []
 
-		// Total: Years
-		{
-			let d1 = new Date(props.dataFile.entries.sort((a, b) => ((a.entryDate || 'zzz') < (b.entryDate || 'zzz') ? -1 : 1))[0].entryDate)
-			let d2 = new Date(props.dataFile.entries.sort((a, b) => ((a.entryDate || '000') > (b.entryDate || '000') ? -1 : 1))[0].entryDate)
-			setTotalYears(d2.getFullYear() - d1.getFullYear() + 1)
-		}
+		props.dataFile.entries
+			.sort((a, b) => (a.entryDate < b.entryDate ? -1 : 1))
+			.forEach((entry) => {
+				entry.dreams.forEach((dream) =>
+					dream.dreamSigns.forEach((sign) => {
+						let tag = tagGroups.filter((tag) => tag.dreamSign === sign)[0]
+						if (tag) {
+							let existingEntry = tag.dailyEntries.filter((item) => item.entryDate == entry.entryDate)[0]
+							if (!existingEntry) tag.dailyEntries.push(entry)
+							tag.totalOccurs++
+						} else {
+							tagGroups.push({ dreamSign: sign, dailyEntries: [entry], totalOccurs: 1 })
+						}
+					})
+				)
 
-		// Total: all metrics
-		{
-			setTotalEntries(props.dataFile.entries.length)
-			setTotalDreams(props.dataFile.entries.map((entry) => entry.dreams.length).reduce((a, b) => a + b))
-			setTotalStars(props.dataFile.entries.filter((entry) => entry.starred).length)
-			setTotalLucid(props.dataFile.entries.map((entry) => entry.dreams.filter((dream) => dream.isLucidDream).length).reduce((a, b) => a + b))
-			setTotalUntagged(props.dataFile.entries.map((entry) => entry.dreams.filter((dream) => dream.dreamSigns.length === 0).length).reduce((a, b) => a + b))
-		}
+				// Find dupes
+				if (
+					entry.dreams &&
+					entry.dreams[0] &&
+					entry.dreams[0].dreamSigns.length > 0 &&
+					entry.dreams[1] &&
+					entry.dreams[0].dreamSigns.toString() == entry.dreams[1].dreamSigns.toString()
+				) {
+					dupeSigns.push(entry)
+				}
 
-		// Tag Groups /&/ dupe tags /&/ bad bedTime
-		{
-			let tagGroups: IDreamSignTagGroup[] = []
-			let dupeSigns: IJournalEntry[] = []
-			let bedTimes: IJournalEntry[] = []
+				// Bad `bedTime` items
+				if (!entry.bedTime || entry.bedTime.length != 5) {
+					bedTimes.push(entry)
+				}
+			})
 
-			props.dataFile.entries
-				.sort((a, b) => (a.entryDate < b.entryDate ? -1 : 1))
-				.forEach((entry) => {
-					entry.dreams.forEach((dream) =>
-						dream.dreamSigns.forEach((sign) => {
-							let tag = tagGroups.filter((tag) => tag.dreamSign === sign)[0]
-							if (tag) {
-								let existingEntry = tag.dailyEntries.filter((item) => item.entryDate == entry.entryDate)[0]
-								if (!existingEntry) tag.dailyEntries.push(entry)
-								tag.totalOccurs++
-							} else {
-								tagGroups.push({ dreamSign: sign, dailyEntries: [entry], totalOccurs: 1 })
-							}
-						})
-					)
-
-					// Find dupes
-					if (
-						entry.dreams &&
-						entry.dreams[0] &&
-						entry.dreams[0].dreamSigns.length > 0 &&
-						entry.dreams[1] &&
-						entry.dreams[0].dreamSigns.toString() == entry.dreams[1].dreamSigns.toString()
-					) {
-						dupeSigns.push(entry)
-					}
-
-					// Bad `bedTime` items
-					if (!entry.bedTime || entry.bedTime.length != 5) {
-						bedTimes.push(entry)
-					}
-				})
-
-			// tagGroup
-			setDreamTagGroups(tagGroups)
-			setTotalDreamSigns(tagGroups.length)
-			// dupe
-			setDupeDreamSigns(dupeSigns)
-			// bedtime
-			setBadBedTimes(bedTimes)
-		}
+		// tagGroup
+		setDreamTagGroups(tagGroups)
+		// dupe
+		setDupeDreamSigns(dupeSigns)
+		// bedtime
+		setBadBedTimes(bedTimes)
 	}, [props.dataFile])
 
 	// -----------------------------------------------------------------------
@@ -200,86 +160,14 @@ export default function TabAdmin(props: Props) {
 
 	// -----------------------------------------------------------------------
 
-	function renderHeader(): JSX.Element {
-		return (
-			<header className='my-5'>
-				<div className='card'>
-					<div className='card-header bg-primary'>
-						<h5 className='card-title text-white mb-0'>Dream Journal Analysis</h5>
-					</div>
-					<div className='card-body bg-light'>
-						<div className='row align-items-start justify-content-around'>
-							<div className='col-auto text-center d-none d-md-block'>
-								<h1 className='text-primary mb-1 x3'>{totalMonths}</h1>
-								<label className='text-primary text-uppercase'>Months</label>
-								<div className='badge rounded-pill bg-primary w-100'>{`${totalYears} years`}</div>
-							</div>
-							<div className='col-auto text-center'>
-								<h1 className='text-primary mb-1 x3'>{totalEntries}</h1>
-								<label className='text-primary text-uppercase'>Days</label>
-								<div className='badge rounded-pill bg-primary w-100'>{totalMonths * 30 > 0 ? (totalEntries / totalMonths).toFixed(2) + ' / mon' : '-'}</div>
-							</div>
-							<div className='col-auto text-center'>
-								<h1 className='text-info mb-1 x3'>{totalDreams}</h1>
-								<label className='text-info text-uppercase d-block'>Dreams</label>
-								<div className='badge rounded-pill bg-info w-100'>{totalMonths * 30 > 0 ? (totalDreams / totalEntries).toFixed(2) + ' / day' : '-'}</div>
-							</div>
-							<div className='w-100 mb-3 d-md-none mb-md-0' />
-							<div className='col-auto text-center'>
-								<h1 className='text-warning mb-1 x3'>{totalStars}</h1>
-								<label className='text-warning text-uppercase d-block'>Starred</label>
-								<div className='badge rounded-pill bg-warning w-100'>
-									{totalDreams && totalStars ? ((totalStars / totalDreams) * 100).toFixed(2) + '%' : '-'}
-								</div>
-							</div>
-							<div className='col-auto text-center'>
-								<h1 className='text-success mb-1 x3'>{totalLucid}</h1>
-								<label className='text-success text-uppercase d-block'>Lucids</label>
-								<div className='badge rounded-pill bg-success w-100'>
-									{totalDreams && totalLucid ? ((totalLucid / totalDreams) * 100).toFixed(2) + '%' : '-'}
-								</div>
-							</div>
-							<div className='col-auto text-center'>
-								<h1 className='text-primary mb-1 x3'>{totalDreamSigns}</h1>
-								<label className='text-primary text-uppercase d-block'>DreamSigns</label>
-								<div className='badge rounded-pill bg-primary w-100'>-</div>
-							</div>
-							<div className='col-auto text-center'>
-								<h1 className='text-info mb-1 x3'>{totalDreams - totalUntagged}</h1>
-								<label className='text-info text-uppercase d-block'>Tagged</label>
-								<div className='badge rounded-pill bg-info w-100'>
-									{totalDreams ? (((totalDreams - totalUntagged) / totalDreams) * 100).toFixed(2) + '%' : '0%'}
-								</div>
-							</div>
-							<div className='col-auto text-center'>
-								<h1 className='text-warning mb-1 x3'>{totalUntagged || '0'}</h1>
-								<label className='text-warning text-uppercase d-block'>Untagged</label>
-								<div className='badge rounded-pill bg-warning w-100'>{totalDreams ? ((totalUntagged / totalDreams) * 100).toFixed(2) + '%' : '0%'}</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</header>
-		)
-	}
-
 	function renderTagGroups(): JSX.Element {
 		return (
 			<section>
-				<div className='row mb-3'>
-					<div className='col'>
-						<h5 className='text-primary'>Search Tags</h5>
-					</div>
+				<div className='row row-cols g-4 align-items-center justify-content-between mb-4' data-desc='commandbar'>
 					<div className='col-auto'>
-						<h5 className='text-primary'>Unique Tags: {dreamTagGroups.length}</h5>
-					</div>
-				</div>
-
-				<div className='row align-items-center border-top border-secondary py-3 mb-3' data-desc='commandbar'>
-					<div className='col-auto d-none d-md-block'>
 						<Search size={40} className='text-secondary' />
 					</div>
-					<div className='col-12 col-md mb-3 mb-md-0'>
+					<div className='col'>
 						<div className='form-floating'>
 							<input
 								id='floatingDreamtag'
@@ -290,7 +178,7 @@ export default function TabAdmin(props: Props) {
 								onChange={(event) => setSearchTerm(event.target.value)}
 								disabled={!props.dataFile ? true : false}
 							/>
-							<label htmlFor='floatingDreamtag'>Search Tags</label>
+							<label htmlFor='floatingDreamtag'>search {dreamTagGroups.length} unique tags</label>
 						</div>
 					</div>
 					<div className='col-auto'>
@@ -334,27 +222,43 @@ export default function TabAdmin(props: Props) {
 					</div>
 				</div>
 
-				<div className='row row-cols-auto g-3 justify-content-between'>
-					{dreamTagGroups
-						.filter((tagGrp) => !searchTerm || tagGrp.dreamSign.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 || searchTerm.indexOf(tagGrp.dreamSign) > -1)
-						.sort((a, b) => {
-							if (filterSortOrder === FilterSortOrder.title) return a.dreamSign.toLowerCase() < b.dreamSign.toLowerCase() ? -1 : 1
-							else if (filterSortOrder === FilterSortOrder.highlow)
-								return a.totalOccurs > b.totalOccurs ? -1 : a.totalOccurs < b.totalOccurs ? 1 : a.dreamSign.toLowerCase() < b.dreamSign.toLowerCase() ? -1 : 1
-							else if (filterSortOrder === FilterSortOrder.lowhigh)
-								return a.totalOccurs < b.totalOccurs ? -1 : a.totalOccurs > b.totalOccurs ? 1 : a.dreamSign.toLowerCase() < b.dreamSign.toLowerCase() ? -1 : 1
-						})
-						.map((tagGrp, idx) => (
-							<div className='col' key={`keyTagGrp${idx}`}>
-								<DreamTagCard
-									setCurrEntry={(entry: IJournalEntry) => setCurrEntry(entry)}
-									setShowModal={(show: boolean) => setShowModal(show)}
-									tagGrp={tagGrp}
-									viewType={filterViewType}
-									doMassUpdateTag={doMassUpdateTag}
-								/>
-							</div>
-						))}
+				<div className='bg-black p-4'>
+					<div className='row row-cols-auto g-3 justify-content-between'>
+						{dreamTagGroups
+							.filter(
+								(tagGrp) => !searchTerm || tagGrp.dreamSign.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 || searchTerm.indexOf(tagGrp.dreamSign) > -1
+							)
+							.sort((a, b) => {
+								if (filterSortOrder === FilterSortOrder.title) return a.dreamSign.toLowerCase() < b.dreamSign.toLowerCase() ? -1 : 1
+								else if (filterSortOrder === FilterSortOrder.highlow)
+									return a.totalOccurs > b.totalOccurs
+										? -1
+										: a.totalOccurs < b.totalOccurs
+										? 1
+										: a.dreamSign.toLowerCase() < b.dreamSign.toLowerCase()
+										? -1
+										: 1
+								else if (filterSortOrder === FilterSortOrder.lowhigh)
+									return a.totalOccurs < b.totalOccurs
+										? -1
+										: a.totalOccurs > b.totalOccurs
+										? 1
+										: a.dreamSign.toLowerCase() < b.dreamSign.toLowerCase()
+										? -1
+										: 1
+							})
+							.map((tagGrp, idx) => (
+								<div className='col' key={`keyTagGrp${idx}`}>
+									<DreamTagCard
+										setCurrEntry={(entry: IJournalEntry) => setCurrEntry(entry)}
+										setShowModal={(show: boolean) => setShowModal(show)}
+										tagGrp={tagGrp}
+										viewType={filterViewType}
+										doMassUpdateTag={doMassUpdateTag}
+									/>
+								</div>
+							))}
+					</div>
 				</div>
 			</section>
 		)
@@ -394,7 +298,9 @@ export default function TabAdmin(props: Props) {
 						<button
 							key={`badTime${idx}`}
 							onClick={(_ev) => {
-								setCurrEntry(entry)
+								let chgEntry = { ...entry }
+								chgEntry.notesPrep += `\n[BEDTIME-FIX]='${chgEntry.bedTime}'`
+								setCurrEntry(chgEntry)
 								setShowModal(true)
 							}}
 							className='btn btn-sm btn-secondary mb-2 me-2'>
@@ -421,10 +327,9 @@ export default function TabAdmin(props: Props) {
 	return !props.dataFile || !props.dataFile.entries ? (
 		<AlertGdriveStatus isBusyLoad={props.isBusyLoad} />
 	) : (
-		<main className='container mb-5'>
+		<main className='container my-auto my-md-5'>
 			<ModalEntry currEntry={currEntry} showModal={showModal} setShowModal={setShowModal} />
-
-			{renderHeader()}
+			<HeaderMetrics dataFile={props.dataFile} isBusyLoad={props.isBusyLoad} showStats={true} />
 
 			<ul className='nav nav-tabs nav-fill' id='adminTab' role='tablist'>
 				<li className='nav-item' role='presentation'>
