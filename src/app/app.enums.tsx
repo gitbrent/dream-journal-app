b78ecb79c893bed56e3e36d171eba49ee592332b
Brent Ely <gitbrent@users.noreply.github.com>
2021-03-09 21:16:12 -0600

    new affirmation

--- src/app/app.enums.tsx
+++ src/app/app.enums.tsx
@@ -8,4 +8,8 @@ export enum NightlyAffirmations {
 		`Tonight, I will recognize my Dream-Signs and become aware I am dreaming.` +
 		`I can see myself becoming aware in my dreams and I can see myself surrounded by Dream-Signs.`,
 	'MILD 2' = `I WILL LUCID DREAM SOON` + `I EXPECT TO LD MUCH MORE OFTEN` + `I love myself and my abilities.` + `I am NOT SCARED of any dream situations.`,
+	'MILD 3' = `I will Lucid Dream tonight.\n` +
+		`I will practice my Kung Fu in the Matrix construct with Morpheus.\n` +
+		`I will spin around in a circle if the dream starts to become unstable.\n` +
+		`I will wake up after 03:00 and perform affirmations, then meditate back to sleep.\n`,
 }
