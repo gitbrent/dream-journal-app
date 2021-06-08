130e727ab7d12cde59042486fbf5bbd86a1e2ca6
Brent Ely <gitbrent@users.noreply.github.com>
2021-06-07 20:58:12 -0500

    formatted

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -89,7 +89,7 @@ class App extends React.Component<IAppProps, IAppState> {
 		console.log(APP_VER)
 	}
 
-	componentDidCatch = (error:any, errorInfo:any) => {
+	componentDidCatch = (error: any, errorInfo: any) => {
 		this.setState({ appErrMsg: error.toString() })
 		console.error(error)
 		console.error(errorInfo)
