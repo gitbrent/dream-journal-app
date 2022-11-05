bd76f82f0f761a8e63e9056db5b2296ce4ff01e2
Brent Ely <gitbrent@users.noreply.github.com>
2022-11-05 16:06:01 -0500

    updated to suppress bootstrap scss warnings

--- webpack.common.js
+++ webpack.common.js
@@ -60,7 +60,13 @@ module.exports = {
 					// Translates CSS into CommonJS
 					"css-loader",
 					// Compiles Sass to CSS
-					"sass-loader",
+					//"sass-loader",
+					{
+						loader: "sass-loader",
+						options: {
+							sassOptions: { quietDeps: true }
+						}
+					}
 				],
 			},
 			{
