574685656856dd216c371047bb3b14102e4485ba
GitBrent <gitbrent@users.noreply.github.com>
2024-09-28 13:16:26 -0500

    fixed/updated SCSS processing

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// CSS - https://github.com/sass/dart-sass/issues/2280#issuecomment-2296302727
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 8080
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern',
+				silenceDeprecations: ['mixed-decls'],
+			},
+		}
 	}
 })
