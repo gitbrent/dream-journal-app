57e182513c5a3de649c3e3376dd2c32a43277767
GitBrent <gitbrent@users.noreply.github.com>
2025-02-14 19:03:35 -0600

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,21 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				api: 'modern',
-				silenceDeprecations: ['mixed-decls'],
+				silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
+				quietDeps: true, // Add this line to suppress warnings (above needed for bootstrap SCSS Dart messages)
+				//api: 'modern',
 			},
+		}
+	},
+	/* following is suppress: `node_modules/gapi-script/gapiScript.js (44:36): Use of eval in "node_modules/gapi-script/gapiScript.js" is [...].` */
+	build: {
+		rollupOptions: {
+			onwarn: (warning, warn) => {
+				if (warning.code === 'EVAL') {  // Suppress eval warnings
+					return;
+				}
+				warn(warning); // Otherwise, show other warnings
+			}
 		}
 	}
 })
