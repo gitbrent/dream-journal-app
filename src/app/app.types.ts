3eb262e182540e652cb856f47263099f13b7e4cf
Brent Ely <gitbrent@users.noreply.github.com>
2026-03-22 16:17:48 -0500

    updated to v2.2.0

--- src/app/app.types.ts
+++ src/app/app.types.ts
@@ -14,8 +14,8 @@
 // @see https://support.google.com/cloud/answer/6158849?hl=en#zippy=%2Cstep-create-a-new-client-secret
 
 // APP
-export const APP_BLD = '20260308-2222'
-export const APP_VER = `2.1.0`
+export const APP_BLD = '20260322-1612'
+export const APP_VER = `2.2.0`
 export const IS_LOCALHOST = window.location.href.toLowerCase().indexOf('localhost') > -1
 export const VERBOSE_IMPORT = true
 
